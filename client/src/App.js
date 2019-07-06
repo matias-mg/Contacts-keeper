1f3d2fbadd3a125036223e5376abf37796e375a3
Chross <matia.medinag@alumnos.duoc.cl>
2019-07-06 18:14:58 -0400

    Context folder created, setting everything

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Navbar from './components/layouts/Navbar'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css'
 
 const App = () => {
   return (
-    <div className="App">
-      My app
-    </div>
+    <Router>
+    <React.Fragment>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/about' component={About}/>
+        </Switch>
+      </div>
+    </React.Fragment>
+    </Router>
   );
 }
 
